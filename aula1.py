eeab414b71f42eea2d63979da048df2dcd2abc13
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2024-06-19 11:59:55 -0300

    Iniciando com classes

---
+++ aula1.py
@@ -0,0 +1,17 @@
+nome = "sivaldo"
+print(type(nome))
+print(nome.capitalize())
+
+
+class TV:
+    #atributos
+    cor = 'preta'
+    tamanho = 55
+    volume = 50
+
+    #metodos
+    def mudar_canal(self):
+        pass
+
+    def mudar_volume(self):
+        pass

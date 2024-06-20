321b71ae77dfa35becb342bf06530b080fc4272d
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2024-06-20 12:47:41 -0300

    Criando metodos para nossa classe

--- contasBanco/contasBanco.py
+++ contasBanco/contasBanco.py
@@ -2,19 +2,38 @@
 class ContaCorrente():
 
      #funcao par criar conta, o que precisa
-    def __init__(self, nome, cpf):
+    def __init__(self, nome, cpf,conta):
         self.nome = nome
         self.cpf = cpf
+        self.conta = conta
         self.saldo = 0
 
+    #metodo consultar saldo
+    def consultar_saldo(self):
+        print(f"Seu saldo atual é de: R${self.saldo:,.2f}")
+
+    def depositar(self, valor):
+        self.saldo += valor
+
+    def sacar_dinheiro(self, valor):
+        self.saldo -= valor
 
 #programa principal
 #criar uma instancia da class ContaCorrente
-conta_sivaldo = ContaCorrente("sivaldo", "111.222.333-44")
+conta_sivaldo = ContaCorrente("sivaldo", "111.222.333-44", "222-34")
 
-print(conta_sivaldo.nome)
-print(conta_sivaldo.cpf)
-print(conta_sivaldo.saldo)
+print(f'Cliente: {conta_sivaldo.nome}')
+print(f'CPF: {conta_sivaldo.cpf}')
+print(f'Conta: {conta_sivaldo.conta}')
+conta_sivaldo.consultar_saldo()
 
+#depositar dinheiro
+conta_sivaldo.saldo= 10000
+conta_sivaldo.consultar_saldo()
+
+
+#sacar dinheiro
+conta_sivaldo.sacar_dinheiro(1000)
+conta_sivaldo.consultar_saldo()
 
 

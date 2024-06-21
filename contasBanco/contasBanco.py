45ad2f35d2e129c0596f49a432645eb9d350640b
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2024-06-21 12:44:25 -0300

    Métodos Estáticos e Modificando Classe da Melhor Forma

--- contasBanco/contasBanco.py
+++ contasBanco/contasBanco.py
@@ -1,5 +1,16 @@
 #classe que vai comandar tudo
+import time
+from datetime import datetime
+import pytz
+
 class ContaCorrente():
+
+    @staticmethod
+    def _data_hora():
+        fuso_BR = pytz.timezone('Brazil/East')
+        horario_BR = datetime.now((fuso_BR))
+        return horario_BR.strftime('%d/%m/%Y %H:%M:%S')
+
 
      #funcao par criar conta, o que precisa
     def __init__(self, nome, cpf, conta, agencia):
@@ -9,27 +20,36 @@ class ContaCorrente():
         self.agencia = agencia
         self.saldo = 0
         self.limite = None
+        self.transacoes = []
 
     #metodo consultar saldo
     def consultar_saldo(self):
         print(f"Seu saldo atual é de: R${self.saldo:,.2f}")
 
-    def depositar(self, valor):
+    def depositar(self, valor): #transacao------
         self.saldo += valor
+        self.transacoes.append((valor, self.saldo, ContaCorrente._data_hora()))
 
     def _limite_conta(self):    #metodo privado  _limite_conta(self) , para se usado somente dentro da classe
         self.limite = -1000
         return self.limite
 
-    def sacar_dinheiro(self, valor):
+    def sacar_dinheiro(self, valor): #transacao----------
         if self.saldo -valor < self._limite_conta():
             print('Saldo insuficiente!')
             self.consultar_saldo()
         else:
             self.saldo -= valor
+            self.transacoes.append((-valor, self.saldo, ContaCorrente._data_hora()))
 
     def consultar_limite_cheque_especial(self):
         print(f'Seu limite de Cheque especial é de: R${self._limite_conta():,.2f}')
+
+    def consultar_historico_transacoes(self):
+        print("Historico de Transações: ")
+        print('Valor, Saldo, Dta e Hora')
+        for transacao in self.transacoes:
+            print(transacao)
 
 #programa principal
 #criar uma instancia da class ContaCorrente
@@ -43,9 +63,10 @@ print(f'Agência: {conta_sivaldo.agencia}')
 conta_sivaldo.consultar_saldo()
 
 #depositar dinheiro
-conta_sivaldo.saldo= 10000
+conta_sivaldo.depositar(10000)
 conta_sivaldo.consultar_saldo()
 
+time.sleep(10)  #ver diferença de tempo (10 segundo)
 #sacar dinheiro
 conta_sivaldo.sacar_dinheiro(10500)
 
@@ -54,4 +75,5 @@ conta_sivaldo.consultar_saldo()
 
 conta_sivaldo.consultar_limite_cheque_especial()
 
-
+print('-'*29)
+conta_sivaldo.consultar_historico_transacoes()
